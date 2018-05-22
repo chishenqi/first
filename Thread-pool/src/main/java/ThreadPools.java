cbeb855a6cf92ad43cfcca204e5867c4654c4d96
BG332387 <sqchi@best-inc.com>
2018-05-22 20:38:12 +0800

    啊擦

---
+++ Thread-pool/src/main/java/ThreadPools.java
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+import org.junit.Test;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by BG332387 on 2018/5/22.
+ */
+public class ThreadPools {
+    ExecutorService executorService = Executors.newSingleThreadExecutor();
+    @Test
+    public void testPost() throws InterruptedException {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            Post post=new Post();
+            Thread t1=new Thread(post);
+            Thread.sleep(1000);
+        }
+    }
+}
