e678fe13b71e824c2574d6aa35d4231bdddd14d9
BG332387 <sqchi@best-inc.com>
2018-05-22 21:21:15 +0800

    a

--- Thread-pool/src/main/java/ThreadPools.java
+++ Thread-pool/src/main/java/ThreadPools.java
@@ -16,12 +16,18 @@ import java.util.concurrent.Executors;
 public class ThreadPools {
     ExecutorService executorService = Executors.newSingleThreadExecutor();
     @Test
-    public void testPost() throws InterruptedException {
+    public void testPostThread() throws InterruptedException {
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
             Post post=new Post();
             Thread t1=new Thread(post);
             Thread.sleep(1000);
         }
+    }
+    @Test
+    public void testPost() throws Exception {
+        ClientWithResponseHandler client=new ClientWithResponseHandler();
+        client.doPost();
+
     }
 }
